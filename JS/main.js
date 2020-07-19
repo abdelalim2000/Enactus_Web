bafc0e6a34965531f86aae8086bca7b8364774d3
Mohamed <mohamed.abdelalilm2000@gmail.com>
2020-07-20 01:00:14 +0200

    init to history

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,17 @@
 /* Nav Logic and animation */
 
-const navbar = document.getElementById("navbar");
-
 window.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
   navbar.classList.toggle("scrolled", window.scrollY > 70);
-  navbar.classList.remove("z-depth-0", window.scrollY > 70);
-  navbar.classList.add("z-depth-2", window.scrollY > 70);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
   var instances = M.Sidenav.init(elems, {});
+});
+
+/* Modal logic */
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems, {});
 });
